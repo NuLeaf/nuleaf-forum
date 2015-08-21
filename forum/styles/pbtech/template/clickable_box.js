b01bc17a003a7eeb826b3426429461894757980b
Wolfgang C. Strack <vurufugangu@gmail.com>
2015-08-20 17:03:21 -0700

    Make entire topic box clickable (correctly this time)

--- forum/styles/pbtech/template/clickable_box.js
+++ forum/styles/pbtech/template/clickable_box.js
@@ -1,7 +1,7 @@
 /*
- * This short jQuery script makes each forum section box clickable after page has
- * loaded by entering the li.row, locating the forum section's link in the forum
- * title, and then changing window location to that link.
+ * This short jQuery script makes each forum section or topic box clickable
+ * after page has loaded by entering the li.row, locating the forum box's link
+ * in the forum title, and then changing window location to that link.
  * --
  * @author Wolfgang C. Strack
  */
@@ -11,5 +11,11 @@
   $sectionBox.css('cursor', 'pointer');
   $sectionBox.click(function() {
     window.location = $(this).find('a.forumtitle').attr('href');
+  });
+
+  var $topicBox = $('li.row.forum-topic');
+  $topicBox.css('cursor', 'pointer');
+  $topicBox.click(function() {
+    window.location = $(this).find('a.topictitle').attr('href');
   });
 })(jQuery);
